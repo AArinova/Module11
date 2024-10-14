6b96b9b1d92edc0323e8aa39a254de9f78acb2cc
AArinova <aliya-arinova@yandex.ru>
2024-10-14 13:02:58 +0700

    Add files via upload

--- module_11_1.py
+++ module_11_1.py
@@ -3,11 +3,11 @@ import numpy as np
 
 df = pd.DataFrame({'values': [val for val in range(1, 101)]})
 
-"""статистические данные числового списка чисел 1..100"""
+"""Статистические данные числового списка чисел 1..100"""
 description = df['values'].describe()
 print(description)
 
-"""чтение из csv- файла"""
+"""Чтение из csv- файла"""
 df = pd.read_csv("file1.txt", sep=",")
 print("\nВывод информации целиком:\n", df)
 
@@ -21,12 +21,18 @@ print(df[df['animal'] == 'tiger'])
 new_df = df[df['water_need'] >= 500]
 print("\nКому требуется больше 500л воды:\n", new_df)
 
-"""Массивы чисел"""
+"""Массивы чисел (numpy)"""
 
-nlist = np.array([val for val in range(1, 101)])
+nlist = np.array([val for val in range(1, 10)])
 print("\nМассив чисел от 1 до 1000:\n", nlist)
+nlist2 = np.sqrt(nlist)
+print("\nМассив все элементы которого являются квадратными корнями предыдущего массива:\n", nlist2)
+print("\nРазность этих массивов:\n", nlist - nlist2)
+print("\nМатричное произведение этих массивов:\n", nlist.dot(nlist2))
 
-rn_list = np.random.random((2,3))
-print("\nМассив случайных чисел:\n", rn_list)
-print("\nСумма всех элементов массива:\t", rn_list.sum())
+rn_list = np.random.random((3,5))
+print("\nМногомерный массив случайных чисел (матрица) размером 3х5:\n", rn_list)
+print("\nСумма всех элементов массива:", rn_list.sum())
+print("\nМинимальное значение массива:", rn_list.min())
+print("\nМаксимальное значение массива:", rn_list.max())
 
